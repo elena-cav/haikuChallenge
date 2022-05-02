129ccc34fbb76b5b187b10b32f061d9c27c3cb49
Elena Cavallero <elenacavallero@outlook.com>
2022-05-02 11:28:23 +0100

    delete listen

--- src/listen.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./text-to-speech";
-
-const server = app.listen(9090, () => {
-  console.log(`Server is listing on port 9090`);
-});
-server.on("error", (e) => console.error("Error", e));
-
-export default app;
